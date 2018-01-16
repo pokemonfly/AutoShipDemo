fcab95a647a7b8bae66d7eeffb395e206fe04ced
yangkai <yangkai>
2018-01-16 16:06:18 +0800

    1

--- main.js
+++ main.js
@@ -1,44 +1,62 @@
+$(init);
+
 var map = [],
     dragType = null;
-$( function () {
-    $( document ).on( 'click', '.create-grid', function () {
-        var w = + $( '#width' ).val(),
-            h = + $( '#height' ).val();
-        createMap( w, h );
-    } ).on( 'dragstart', '.drag-item', function ( e ) {
-        dragType = $( this ).attr( 'data-type' );
-        console.log(dragType)
-    } ).on( 'dragover', '.ceil', function ( e ) {
-        // $(this).addClass('drag-over')
-        e.preventDefault();
-        e.stopPropagation();
-    } ).on( 'drop', '.ceil', function ( e ) {
-        e.preventDefault();
-        e.stopPropagation();
-        var $t = $( this ),
-            row = $t.attr( 'data-row' ),
-            col = $t.attr( 'data-col' );
-        $( this ).addClass( 'ceil-ship ceil-type' + dragType )
-    } )
-} )
 
-function createMap( w, h ) {
-    map = Array( h + 1 ).fill( Array( w + 1  ).fill( 0 ) )
-    var html = map.map( function ( row, rowIndex ) {
-        var rowHtml = row.map( function ( col, colIndex ) {
+function init() {
+    $(document).on('click', '.create-grid', function() {
+        var w = +$('#width').val(),
+            h = +$('#height').val();
+        createMap(w, h);
+    }).on('dragstart', '.drag-item', function(e) {
+        dragType = $(this).attr('data-type');
+        console.log(dragType)
+    }).on('dragover', '.ceil', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+    }).on('drop', '.ceil', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        var $t = $(this),
+            row = $t.attr('data-row'),
+            col = $t.attr('data-col');
+        $(this).addClass('ceil-ship ceil-type' + dragType)
+    })
+    .on('click', '.create-grid-load', load)
+    .on('click', '.update', update)
+    .on('click', '.calc', calc)
+    .on('click', '.move', move)
+}
+
+function createMap(w, h) {
+    map = Array(h + 1).fill(Array(w + 1).fill(0))
+    var html = map.map(function(row, rowIndex) {
+        var rowHtml = row.map(function(col, colIndex) {
             if (!rowIndex) {
-                var str = colIndex > 0 ?  String.fromCharCode(64 + colIndex) : ''
-                return '<div class="ceil label">' + str  + '</div>'
+                var str = colIndex > 0 ? String.fromCharCode(64 + colIndex) : ''
+                return '<div class="ceil label">' + str + '</div>'
             }
-            if (!colIndex ) {
-                return '<div class="ceil label">' + rowIndex  + '</div>'
+            if (!colIndex) {
+                return '<div class="ceil label">' + rowIndex + '</div>'
             }
             return `<div id="${ rowIndex}-${ colIndex}" data-row="${ rowIndex}" data-col="${ colIndex }"  class="ceil"></div>`
-        } ).join( '' )
+        }).join('')
         return `<div class="row">${ rowHtml }</div>`
-    } ).join( '' )
-    $( '.grid' ).html( html )
+    }).join('')
+    $('.grid').html(html)
 }
-function addTarget( pos, type ) {}
 
-function aStar( start, end ) {}
+function addTarget(pos, type) {}
+
+function aStar(start, end) {
+    return [];
+    return null
+}
+
+function load() {}
+
+function update() {}
+
+function calc() {}
+
+function move() {}
